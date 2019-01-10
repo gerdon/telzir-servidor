3fdbf93585629c9eb4408f71cc1557a234e0d5ca
Gerdon Mafra <gerdon_mafra@hotmail.com>
2019-01-10 03:06:20 -0300

    Calcular Ligação implementado

--- app/controllers/planos.controller.js
+++ app/controllers/planos.controller.js
@@ -1,6 +1,79 @@
 
-function calcularLigacao() {
+function calcularLigacao(origem, destino, tempo, plano) {
+    let localizacao = origem + destino;
+    let valorMinuto;
+    let valorComPlano;
+    let valorSemPlano;
 
+    switch (localizacao) {
+        case '011016':
+            valorMinuto = 1.90;
+            valorComPlano = 0;
+            valorSemPlano = tempo * valorMinuto;
+
+            if(tempo > plano) {
+                valorComPlano = (tempo - plano) * (valorMinuto + (valorMinuto * 0.1));
+            }
+
+            break;
+        case '011017':
+            valorMinuto = 1.70;
+            valorComPlano = 0;
+            valorSemPlano = tempo * valorMinuto;
+
+            if(tempo > plano) {
+                valorComPlano = (tempo - plano) * (valorMinuto + (valorMinuto * 0.1));
+            }
+
+            break;
+        case '011018':
+            valorMinuto = 0.90;
+            valorComPlano = 0;
+            valorSemPlano = tempo * valorMinuto;
+
+            if(tempo > plano) {
+                valorComPlano = (tempo - plano) * (valorMinuto + (valorMinuto * 0.1));
+            }
+
+            break;
+        case '016011':
+            valorMinuto = 2.90;
+            valorComPlano = 0;
+            valorSemPlano = tempo * valorMinuto;
+
+            if(tempo > plano) {
+                valorComPlano = (tempo - plano) * (valorMinuto + (valorMinuto * 0.1));
+            }
+
+            break;
+        case '017011':
+            valorMinuto = 2.70;
+            valorComPlano = 0;
+            valorSemPlano = tempo * valorMinuto;
+
+            if(tempo > plano) {
+                valorComPlano = (tempo - plano) * (valorMinuto + (valorMinuto * 0.1));
+            }
+
+            break;
+        case '018011':
+            valorMinuto = 1.90;
+            valorComPlano = 0;
+            valorSemPlano = tempo * valorMinuto;
+
+            if(tempo > plano) {
+                valorComPlano = (tempo - plano) * (valorMinuto + (valorMinuto * 0.1));
+            }
+
+            break;
+        default:
+            break;
+    }
+
+    return {
+        "valorComPlano": valorComPlano,
+        "valorSemPlano": valorSemPlano
+    };
 }
 
 module.exports = {calcularLigacao};
--- app/routes/planos.router.js
+++ app/routes/planos.router.js
@@ -2,7 +2,12 @@ const router = require('../../config/server').server;
 const planos = require('../controllers/planos.controller');
 
 router.post('/calcularLigacao', (req, res) => {
-    let valorCalculado = planos.calcularLigacao(req.body);
+    let valorCalculado = planos.calcularLigacao(
+        req.body.origem, 
+        req.body.destino, 
+        req.body.tempo, 
+        req.body.plano
+    );
     res.send(valorCalculado);
 });
 
